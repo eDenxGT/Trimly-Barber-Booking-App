df64369543f97fb4e99c415f328c8299fffe244b
eDenxGT <edenxgt@gmail.com>
2025-03-19 10:58:47 +0530

    refactor: barber shop schema updated

--- trimly-api/src/entities/models/barber-shop.entity.ts
+++ trimly-api/src/entities/models/barber-shop.entity.ts
@@ -1,12 +1,31 @@
-export interface IBarberShopEntity {
-	id?: string;
+export interface IBarberShop {
+	id: string;
 	name: string;
-	ownerId: string;
-	barberIds: string[];
-	paymentMode: "wallet" | "direct";
+	owner: string; 
+	barbers: string[]; 
+	paymentMode: "shop_wallet" | "direct_payment";
+	address: {
+		street?: string;
+		city?: string;
+		state?: string;
+		country?: string;
+		zipCode: string;
+		location: {
+			latitude: number;
+			longitude: number;
+		};
+	};
 	commissionPercentage: number;
 	walletBalance: number;
-	barberEarnings: Record<string, number>;
+	barberEarnings?: Record<string, number>; 
+	openingHours: {
+		day: string;
+		open?: string;
+		close?: string;
+		closed?: boolean;
+	}[];
+	createdBy: string; 
+	approvedBy?: string; 
 	createdAt?: Date;
 	updatedAt?: Date;
 }
--- trimly-api/src/frameworks/database/mongoDB/schemas/barber-shop.schema.ts
+++ trimly-api/src/frameworks/database/mongoDB/schemas/barber-shop.schema.ts
@@ -13,8 +13,19 @@ export const barberShopSchema = new Schema(
 		barbers: [{ type: String }],
 		paymentMode: {
 			type: String,
-			enum: ["wallet", "direct"],
+			enum: ["shop_wallet", "direct_payment"],
 			default: "wallet",
+		},
+		address: {
+			street: { type: String },
+			city: { type: String },
+			state: { type: String },
+			country: { type: String },
+			zipCode: { type: String, required: true },
+			location: {
+				latitude: { type: Number, required: true },
+				longitude: { type: Number, required: true },
+			},
 		},
 		commissionPercentage: {
 			type: Number,
@@ -29,6 +40,27 @@ export const barberShopSchema = new Schema(
 			of: Number,
 			default: {},
 		},
+		openingHours: {
+			type: [
+				{
+					day: { type: String, required: true },
+					open: { type: String },
+					close: { type: String },
+					closed: { type: Boolean, default: false },
+				},
+			],
+			default: [
+				{ day: "Monday", open: "9:00 AM", close: "9:00 PM" },
+				{ day: "Tuesday", open: "9:00 AM", close: "9:00 PM" },
+				{ day: "Wednesday", open: "9:00 AM", close: "9:00 PM" },
+				{ day: "Thursday", open: "9:00 AM", close: "9:00 PM" },
+				{ day: "Friday", open: "9:00 AM", close: "9:00 PM" },
+				{ day: "Saturday", open: "9:00 AM", close: "9:00 PM" },
+				{ day: "Sunday", closed: true },
+			],
+		},
+		createdBy: { type: String, required: true },
+		approvedBy: { type: String },
 	},
 	{ timestamps: true }
 );
